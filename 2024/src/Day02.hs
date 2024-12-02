1f0686596b9203a397c83af9a2a71d42ebbea69e
Luke Washak <lukewashak@gmail.com>
2024-12-02 22:07:06 +0000

    Day 2 Part 2

--- 2024/src/Day02.hs
+++ 2024/src/Day02.hs
@@ -6,12 +6,16 @@ type Report = [Int]
 
 part1 :: String -> Int
 part1 input = let rs = map parseReport (lines input)
-                  as = map isSafeIncreasing rs
-                  bs = map isSafeDecreasing rs
+                  as = map (isSafe safeIncrease) rs
+                  bs = map (isSafe safeDecrease) rs
               in sum [if s then 1 else 0 | s <- zipWith (||) as bs]
 
-part2 :: String -> String
-part2 _ = "TODO"
+
+part2 :: String -> Int
+part2 input = let rs = map parseReport (lines input)
+                  as = map (isSafeWithDampener safeIncrease) rs
+                  bs = map (isSafeWithDampener safeDecrease) rs
+              in sum [if s then 1 else 0 | s <- zipWith (||) as bs]
 
 
 parseReport :: String -> Report
@@ -21,16 +25,34 @@ parseReport cs = case C.getFirstInt cs of
                       (Nothing, cs')  -> parseReport cs'
 
 
-isSafe :: (Int -> Int -> Int) -> Report -> Bool
+isSafe :: (Int -> Int -> Bool) -> Report -> Bool
 isSafe _ []  = True
-isSafe _ (_:[]) = True
-isSafe delta (x1:x2:xs) = let d = delta x1 x2
-                              safe = d <= 3 && d > 0
-                          in safe && isSafe delta (x2:xs)
+isSafe _ [_] = True
+isSafe safe (x1:x2:xs) = safe x1 x2 && isSafe safe (x2:xs)
 
 
-isSafeIncreasing :: Report -> Bool
-isSafeIncreasing = isSafe (flip (-))
+safeIncrease :: Int -> Int -> Bool
+safeIncrease x y = safeDelta (y - x)
 
-isSafeDecreasing :: Report -> Bool
-isSafeDecreasing = isSafe (-)
+
+safeDecrease :: Int -> Int -> Bool
+safeDecrease x y = safeDelta (x - y)
+
+
+safeDelta :: Int -> Bool
+safeDelta d = d <= 3 && d > 0
+
+
+isSafeWithDampener :: (Int -> Int -> Bool) -> Report -> Bool
+isSafeWithDampener = isSafeWithDampener' False Nothing
+
+
+isSafeWithDampener' :: Bool -> Maybe Int -> (Int -> Int -> Bool) -> Report -> Bool
+isSafeWithDampener' _ _ _ []  = True
+isSafeWithDampener' _ _ _ (_:[]) = True
+isSafeWithDampener' prevFailure mx0 safe (x1:x2:xs)
+    | safe x1 x2  = isSafeWithDampener' prevFailure (Just x1) safe (x2:xs)
+    | prevFailure = False
+    | otherwise   = case mx0 of
+                        Just x0 -> isSafeWithDampener' True Nothing safe (x0:x2:xs) || isSafeWithDampener' True Nothing safe (x0:x1:xs)
+                        Nothing -> isSafeWithDampener' True Nothing safe (x2:xs)    || isSafeWithDampener' True Nothing safe (x1:xs)
