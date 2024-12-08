1908b4f0b9187ad1dd7e0e588df5c64ff6d016f1
Luke Washak <lukewashak@gmail.com>
2024-12-08 11:51:13 +0000

    Day 8 Part 2

--- 2024/src/Day08.hs
+++ 2024/src/Day08.hs
@@ -2,7 +2,6 @@ module Day08 (main) where
 
 import Common (findIndices2D)
 
-import Data.Char (isLower, isUpper, isDigit)
 import Data.List (nub)
 
 
@@ -16,43 +15,57 @@ type Output = Int
 -- Parsing
 parseInput :: String -> Input
 parseInput raw = let grid = lines raw
-                     lowers = findIndices2D isLower grid
-                     uppers = findIndices2D isUpper grid
-                     digits = findIndices2D isDigit grid
+                     chars = nub $ concatMap (filter (/= '.')) grid
+                     antennas = map (\c -> findIndices2D (==c) grid) chars
                      height = length grid
                      width  = length $ head grid
-                 in ([lowers, uppers, digits], (width, height))
+                 in (antennas, (width, height))
 
 -- Solutions
-partOne :: Input -> Int
-partOne (pss, bounds) = length $ nub $ foldMap (getAntinodes bounds) pss
-
-getAntinodes :: (Int, Int) -> [Point] -> [Point]
-getAntinodes b as = let antennaPairs = [ (p1,p2) | p1 <- as, p2 <- as, p1 < p2 ]
-                        antennaMirrors = foldMap (uncurry getMirrorPoints) antennaPairs
-                        antinodes = filter (\p -> not (isOutOfBounds b p) && p `notElem` as) antennaMirrors
-                    in antinodes
-
-getMirrorPoints :: Point -> Point -> [Point]
-getMirrorPoints (i1,j1) (i2,j2) =
-    let d1 = (i2 - i1, j2 - j1)
-        m1 = (i1 - fst d1, j1 - snd d1)
-        m2 = (i2 + fst d1, j2 + snd d1)
-    in [m1, m2]
-
-isOutOfBounds :: (Int, Int) -> Point -> Bool
-isOutOfBounds (w,h) (i,j) = i < 0 || j < 0 || i >= w || j >= h
+partOne :: Input -> Output
+partOne (pss, bounds) = length $ nub $ foldMap (getAntinodes (getMirrorPoints bounds)) pss
 
 partTwo :: Input -> Output
-partTwo _ = 2
+partTwo (pss, bounds) = length $ nub $ foldMap (getAntinodes (getResonantPoints bounds)) pss
 
+getAntinodes :: (Point -> Point -> [Point]) -> [Point] -> [Point]
+getAntinodes f as = let antennaPairs = [ (p1,p2) | p1 <- as, p2 <- as, p1 < p2 ]
+                        antinodes = foldMap (uncurry f) antennaPairs
+                    in antinodes
+
+getMirrorPoints :: (Int, Int) -> Point -> Point -> [Point]
+getMirrorPoints b p1 p2 =
+    let d  = p1 `subtractPoint` p2
+        m1 = p1 `addPoint` d
+        m2 = p2 `subtractPoint` d
+    in filter (isInBounds b) [m1, m2]
+
+getResonantPoints :: (Int, Int) -> Point -> Point -> [Point]
+getResonantPoints b p1 p2 =
+    let d   = p1 `subtractPoint` p2
+        ds  = map (`multiplyPoint` d) [0..]
+        rs1 = takeWhile (isInBounds b) $ map (addPoint p1) ds
+        rs2 = takeWhile (isInBounds b) $ map (subtractPoint p2) ds
+    in rs1 ++ rs2
+
+addPoint :: Point -> Point -> Point
+addPoint (i1,j1) (i2,j2) = (i1 + i2, j1 + j2)
+
+subtractPoint :: Point -> Point -> Point
+subtractPoint (i1,j1) (i2,j2) = (i1 - i2, j1 - j2)
+
+multiplyPoint :: Int -> Point -> Point
+multiplyPoint n (i,j) = (i * n, j * n)
+
+isInBounds :: (Int, Int) -> Point -> Bool
+isInBounds (w,h) (i,j) = i >= 0 && j >= 0 && i < w && j < h
 
 
 -- Main
 main :: IO ()
 main = do
-    raw <- readFile "../input/example/Day08.txt"
+    raw <- readFile "../input/Day08.txt"
     let input = parseInput raw
     print input
-    print $ partOne input  -- 2315 (high), 2290 (high)
+    print $ partOne input
     print $ partTwo input
