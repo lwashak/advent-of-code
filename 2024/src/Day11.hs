8d4b3957b4f7264e9386c798ef53092717075b19
Luke Washak <lukewashak@gmail.com>
2024-12-11 20:59:17 +0000

    Day 11

--- 2024/src/Day11.hs
+++ 2024/src/Day11.hs
@@ -1,29 +1,48 @@
 module Day11 (Input, Output, parseInput, partOne, partTwo, main) where
 
+-- import qualified Control.Parallel.Strategies as P
+
 import Parsing (readInts)
-import Common (applyN)
+-- import Common (applyN)
+
+import Data.List (group, sort, splitAt)
+
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as M
 
 -- Types
-type Input = [Int]
+type Input = IntMap Int
 type Output = Int
 
 -- Parsing
 parseInput :: String -> Input
-parseInput = readInts
+parseInput raw = let xss = group $ sort $ readInts raw
+                     occurances = zip (map head xss) (map length xss)
+                 in M.fromList occurances
 
 -- Solutions
 partOne :: Input -> Output
-partOne xs = length $ applyN 25 blink xs
+partOne = sum . M.elems . (!! 25) . iterate blink
 
 partTwo :: Input -> Output
-partTwo xs = length $ applyN 75 blink xs
+partTwo = sum . M.elems . (!! 75) . iterate blink
 
-blink :: [Int] -> [Int]
-blink [] = []
-blink (x:xs)
-    | x == 0               = 1 : blink xs
-    | even (length digits) = map digitsToInt (splitList digits) ++ blink xs
-    | otherwise            = x * 2024 : blink xs
+blink :: IntMap Int -> IntMap Int
+blink stones = fst $ M.mapAccumWithKey go M.empty stones
+    where go stones' stone count = case mutateStone stone of
+                                        [s]      -> (M.alter (addStone count) s stones', stones)
+                                        [s1, s2] -> (M.alter (addStone count) s1 $ M.alter (addStone count) s2 stones', stones)
+                                        _ -> error "Expecting no more than 2 stones"
+
+addStone :: Int -> Maybe Int -> Maybe Int
+addStone count' Nothing     = Just count'
+addStone count' (Just count)  = Just (count + count')
+
+mutateStone :: Int -> [Int]
+mutateStone x
+    | x == 0               = [1]
+    | even (length digits) = splitStones digits
+    | otherwise            = [x * 2024]
     where digits = intToDigits x
 
 intToDigits :: Int -> [Int]
@@ -33,7 +52,7 @@ intToDigits x
     | x >= 10 = let d1 = x `div` 10
                     d2 = x - (d1 * 10)
                 in intToDigits d1 ++ [d2]
-    | otherwise = error "Impossible"
+intToDigits _ = error "Impossible"
 
 digitsToInt :: [Int] -> Int
 digitsToInt = digitsToInt' 0
@@ -43,11 +62,9 @@ digitsToInt' acc [] = acc
 digitsToInt' acc (x:xs) = digitsToInt' acc' xs
     where acc' = (acc * 10) + x
 
-splitList :: [a] -> [[a]]
-splitList xs = let mid = (length xs) `div` 2
-                   as  = take mid xs
-                   bs  = drop mid xs
-               in [as, bs]
+splitStones :: [Int] -> [Int]
+splitStones xs = map digitsToInt [l, r]
+    where (l,r) = splitAt (length xs `div` 2) xs
 
 -- Main
 main :: IO ()
